e9a4f3162395a7da2225daad577e50a5fdc66dfb
St3f4n-d3v <89920061+St3f4n-d3v@users.noreply.github.com>
2021-10-19 14:38:05 +0200

    Add files via upload

---
+++ opdracht_12_java/script.js
@@ -0,0 +1,10 @@
+console.log("Hallo Winc Academy studenten");
+//dit is een grote som
+let a = 1230941*1823794;
+console.log (a);
+let b = 637263/54
+console.log(b)
+/*let a = 1230941*1823794;
+console.log (a);
+let b = 637263/54
+console.log(b)*/
