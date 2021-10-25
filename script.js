cc3b2adacd9cbaa6ecb5956c49e4c94a10abf466
St3f4n-d3v <89920061+St3f4n-d3v@users.noreply.github.com>
2021-10-25 15:13:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+
+const age = 17;
+
+var inputAge = prompt("Wat is uw leeftijd?");
+
+if (inputAge > age) {
+
+    document.write("Je mag naar binnen.");
+} else {
+
+    document.write("We mogen je helaas niet binnen laten.");
+}
+
+console.log
+
+const isFemale = vrouw = true;
+const isMale = man = false
+
+var inputGender = prompt("wat is uw geslacht?");
+
+if (inputGender = vrouw) {
+
+    document.write("U mag een kaartje kopen.");
+
+}
+
+if (inputGender = man) {
+
+    document.write("U mag geen kaartje kopen.");
+}
+
+console.log
+
+const driverStatus = 'bob'
+var inputBob = prompt("Bent u bob?")
+
+if (bob = true) {
+
+    document.write("U mag rijden")
+} else {
+
+    document.write("U mag niet rijden")
+}
+
+
+console.log
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
