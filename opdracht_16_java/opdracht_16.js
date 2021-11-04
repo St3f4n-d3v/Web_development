22ff281fb7e933b7bccdf54c267d7e2d0f28e686
St3f4n-d3v <89920061+St3f4n-d3v@users.noreply.github.com>
2021-11-04 15:27:11 +0100

    Add files via upload

---
+++ opdracht_16_java/opdracht_16.js
@@ -0,0 +1,28 @@
+
+let makePizza = function () {
+    console.log('1. Heat the oven');
+    console.log('2. Take dough');
+    console.log('3. Flatten the dough');
+    console.log('4. Spread tomato sauce evenly over the pizzadough');
+    console.log('5. Make a selection of vegetables and herbs, put them on the pizza top it with cheese');
+    console.log('6. Slide the pizza in the oven and bake for 15 minutes');
+    console.log('7. Take the pizza from the oven and let it cool off for 3 minutes before slicing');
+};
+
+let makeSushi = function () {
+    console.log('1. Boil sushi rice in the rice cooker');
+    console.log('2. Heat spices like; vinegar, sugar, and salt together in a pot');
+    console.log('3. Cut and prepare filling ingredients');
+    console.log('4. Place a sheet of nori on the rolling mat');
+    console.log('5. Spread a layer of sushi rice on the sheet of nori');
+    console.log('6. Add filling ingredients to the rice');
+    console.log('7. Roll the sushi using your mat as a guide');
+    console.log('8. Cut your roll into 2 cm pieces');
+};
+
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
